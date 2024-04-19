6d18580094dbc5286e92a85d17f771f8f6865713
gustavorods <gustavo.rlsilva07@gmail.com>
2024-04-19 15:07:41 -0300

    delete folder 'tecnico'

--- Tecnico/DS1/ExercicioDeIntroduçãoPOOJava/exemplo2/src/exemplo2.java
+++
@@ -1,9 +0,0 @@
-public class exemplo2 {
-    public static void main(String[] args) {
-        // Instanciação do objeto
-        Usuario usuario1 = new Usuario("teste@gmail", "", "Gustavo", "");
-
-        // Chamada ao provarExistencia
-        usuario1.provaExistencia();
-    }
-}
